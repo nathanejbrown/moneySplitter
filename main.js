e573283125394c6d373189693f9ff46cd48af09a
Heathcliff <dennis.nathan@gmail.com>
2017-05-09 12:46:34 -0600

    Validate function is returning the correct values

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 function moneySplitter (total) {
   var valuesArray = divideIntoThree(total);
+  console.log(validate(valuesArray, total, 0));
 }
 
 function divideIntoThree (initalValue) {
   var a = b = c = +((initalValue/3).toFixed(2));
   return [a, b, c];
 }
+
+function validate (array, total, xValue) {
+  let operator = '';
+  var sum = array.reduce(function add(a, b) {
+    return a + b;
+  }, 0);
+
+  if (sum === total) {
+    return array;
+  }
+
+  if (sum > total) {
+    array[xValue] -= .01;
+  } else if (sum < total) {
+    array[xValue] += .01;
+  }
+  if (xValue === array.length) {
+    xValue = 0;
+  } else xValue++;
+  return validate(array, total, xValue);
+}
+
+moneySplitter(800.0);
